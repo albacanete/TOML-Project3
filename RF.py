f8491e2c8828b369ba5b8651671a52b7dfd203ea
albacg5 <albasmedia@protonmail.com>
2022-06-21 18:05:15 +0200

    finish RF cross

--- RF.py
+++ RF.py
@@ -2,61 +2,95 @@ import pandas as pd
 import data
 import utils
 import matplotlib.pyplot as plt
-import numpy as np
+import math
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split, cross_val_score
 import seaborn as sns
 
 
-def random_forest(x_train, y_train, x_test, y_test):
+def random_forest(x, y):
+    # divide dataset
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=1)
+
     pred = pd.DataFrame()
     pred['RefSt'] = y_test
     pred['Sensor_O3'] = x_test['Sensor_O3']
     pred['date'] = data.new_PR_data_inner['date']
 
-    rf = RandomForestRegressor()
-
-    estimators_rf = np.linspace(1, 20, num=5, dtype=int)
+    n_trees = [1, 2, 5, 7, 10, 13, 15, 18, 20, 25]
 
     r2 = []
-    rmse = []
+    mse = []
     mae = []
 
-    for n in estimators_rf:
-        rf.set_params(n_estimators=n)
+    for n in n_trees:
+        rf = RandomForestRegressor(n_estimators=n)
         rf.fit(x_train, y_train)
-        prediction_rf = rf.predict(x_test)
+        r2_scores = cross_val_score(rf, x_train, y_train, cv=10, scoring='r2')
+        mae_score = cross_val_score(rf, x_train, y_train, cv=10, scoring='neg_mean_absolute_error')
+        mse_score = cross_val_score(rf, x_train, y_train, cv=10)
+        r2.append(r2_scores.mean())
+        mse.append(mse_score.mean())
+        mae.append(mae_score.mean())
 
-        pred['RF_Prediction'] = prediction_rf
+    rmse = [math.sqrt(1 - x) for x in mse]
+    mae = [-1 * x for x in mae]
+    print(r2)
+    print(rmse)
+    utils.table_creation(['Number of trees', 'R^2', 'RMSE', 'MAE'], [n_trees, r2, rmse, mae],
+                         'rf_table.txt')
 
-        print(n, prediction_rf)
-        print("RANDOM FOREST WITH " + str(n) + " TREES")
-        print("R²: " + str(r2_score(y_test, prediction_rf)))
-        r2.append(r2_score(y_test, prediction_rf))
-        print("RMSE: " + str(mean_squared_error(y_test, prediction_rf, squared=False)))
-        rmse.append(mean_squared_error(y_test, prediction_rf, squared=False))
-        print("MAE: " + str(mean_absolute_error(y_test, prediction_rf)))
-        mae.append(mean_absolute_error(y_test, prediction_rf))
-        print()
+    # plot errors
+    plt.title("R-squared")
+    plt.xlabel('Number of neighbors')
+    plt.ylabel('R^2')
+    plt.plot(n_trees, r2, color='red')
+    plt.savefig("img/rf_r2")
+    plt.clf()
 
-        ax1 = pred.plot(x='date', y='RefSt')
-        pred.plot(x='date', y='RF_Prediction', ax=ax1, title='Random Forest for ' + str(n) + ' trees.')
-        plt.show()
+    plt.title("Root Mean Squared Error")
+    plt.xlabel('Number of neighbors')
+    plt.ylabel('RMSE')
+    plt.plot(n_trees, rmse, color='blue')
+    plt.savefig("img/rf_rmse")
+    plt.clf()
 
-        sns_rf = sns.lmplot(x='RefSt', y='RF_Prediction', data=pred, fit_reg=True,
-                            line_kws={'color': 'orange'}).set(title='Random Forest for ' + str(n) + ' trees.')
-        sns_rf.set(ylim=(-2, 3))
-        sns_rf.set(xlim=(-2, 3))
-        plt.show()
+    plt.title("Mean Absoulte Error")
+    plt.xlabel('Number of neighbors')
+    plt.ylabel('MAE')
+    plt.plot(n_trees, mae, color='black')
+    plt.savefig("img/rf_mae")
+    plt.clf()
 
-    utils.table_creation(['Number of trees', 'R^2', 'RMSE', 'MAE'], [estimators_rf, r2, rmse, mae],
-                         'random_forest_table.txt')
+    # best number of trees is the one with higher R^2
+    best_n = n_trees[r2.index(max(r2))]
+    print(best_n)
 
-    plt.title("Random Forest. Metrics vs  number of estimators (trees)")
-    plt.xlabel('Number of trees')
-    plt.ylabel('Metric value')
-    plt.plot(estimators_rf, r2, color='red', label="R^2")
-    plt.plot(estimators_rf, rmse, color='blue', label="RMSE")
-    plt.plot(estimators_rf, mae, color='green', label="MAE")
-    plt.legend(loc="center left")
-    plt.show()
+    model = RandomForestRegressor(n_estimators=best_n)
+    model.fit(x_train, y_train)
+    mpred = model.predict(x_test)
+    acc = model.score(x_test, y_test)
+    pred['Pred'] = mpred
+
+    print("RF PREDICTION")
+    print("R^2: ", str(r2_score(y_test, mpred)))
+    print("RMSE: ", str(mean_squared_error(y_test, mpred, squared=False)))
+    print("MAE: ", str(mean_absolute_error(y_test, mpred)))
+    print("Accuracy: ", str(acc * 100))
+
+    ax1 = pred.plot(x='date', y='RefSt', color='red')
+    pred.plot(x='date', y='Pred', ax=ax1, title='Random Forest for ' + str(best_n) + ' trees.', color='blue')
+    label = "RF_" + str(best_n)
+    plt.savefig("img/" + label)
+    plt.clf()
+
+    # sns.set(rc={"figure.figsize": (12, 15)})
+    sns_rf = sns.lmplot(x='RefSt', y='Pred', data=pred, fit_reg=True, height=5, aspect=1.5,
+                        line_kws={'color': 'orange'})
+    sns_rf.fig.suptitle('Random Forest for ' + str(best_n) + ' trees.')
+    sns_rf.set(ylim=(-2, 3))
+    sns_rf.set(xlim=(-2, 3))
+    label = "RF_line_" + str(best_n)
+    plt.savefig("img/" + label)
+    plt.clf()
